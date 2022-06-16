fa8004f2bf19c210b9f3895d30b65c5fbaaa79cd
Chiara Bonsignore <chiarabonsignore@JackBoodiChiara.lan>
2022-06-16 11:03:53 +0200

    Aggiornate le info e aggiunto l'icona

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,60 @@
-import { AssistantPackage, RuleDefinition } from '@sketch-hq/sketch-assistant-types'
+import {
+  AssistantPackage,
+  RuleDefinition,
+} from '@sketch-hq/sketch-assistant-types'
 
-const helloWorld: RuleDefinition = {
+const duplicateSymbols: RuleDefinition = {
   rule: async (context) => {
-    context.utils.report('Hello world')
+    interface Duplicate {
+      name: string
+      number: number
+      local: boolean
+      foreign: boolean
+      symbol: any
+    }
+
+    var duplicates: Array<Duplicate> = [];
+
+    for (const symbol of context.utils.objects.symbolMaster) {
+      var existingElement = duplicates.find((element) => element.name == symbol.name);
+      if (existingElement != null)
+        existingElement.number++;
+      else
+        duplicates.push({ name: symbol.name, number: 1, local: true, foreign: false, symbol });
+    }
+
+    for (const msimmutablesymbol of context.utils.foreignObjects.MSImmutableForeignSymbol) {
+      var symbol = msimmutablesymbol.symbolMaster;
+      var existingElement = duplicates.find((element) => element.name == symbol.name)
+      if (existingElement != null) {
+        existingElement.number++;
+        existingElement.foreign = true;
+      }
+      else {
+        duplicates.push({ name: symbol.name, number: 1, local: false, foreign: true, symbol })
+      }
+    }
+
+    for (const duplicate of (duplicates.filter((element) => element.number > 1))) {
+      context.utils.report(`Symbol ${duplicate.name} duplicate ${duplicate.number} times`);
+      context.utils.report("", duplicate.symbol);
+    }
   },
-  name: 'sketch-assistant-template/hello-world',
-  title: 'Hello World',
-  description: 'Reports a hello world message',
+  name: 'duplicate-symbols-finder/duplicate-symbols',
+  title: 'Duplicate symbols',
+  description: 'Reports duplicate symbols in your design file.',
 }
 
 const assistant: AssistantPackage = async () => {
   return {
-    name: 'sketch-assistant-template',
-    rules: [helloWorld],
+    name: 'symbols-no-duplicated',
+    rules: [duplicateSymbols],
+    title: (`Symbols should not be duplicated`),
+    description: (`Duplicated symbols could cause inconsistencies and make updating components hard to manage.`
+    ),
     config: {
       rules: {
-        'sketch-assistant-template/hello-world': { active: true },
+        'duplicate-symbols-finder/duplicate-symbols': { active: true },
       },
     },
   }
